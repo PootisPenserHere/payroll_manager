120e07b31535e8353846d293386c1035c01c0231
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-08-05 20:50:33 -0600

    [add] cors

--- api-payroll/src/middleware.php
+++ api-payroll/src/middleware.php
@@ -2,3 +2,21 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+// Enable cors
+$app->add(new \Tuupola\Middleware\Cors([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => ["Accept", "Content-Type"],
+    "headers.expose" => [],
+    "credentials" => false,
+    "cache" => 0,
+    "logger" => $container['logger'],
+    "error" => function ($request, $response, $arguments) {
+        $data["status"] = "error";
+        $data["message"] = $arguments["message"];
+        return $response
+            ->withHeader("Content-Type", "application/json")
+            ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    }
+]));
