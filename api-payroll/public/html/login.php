d4135188bd06c79f414d6727f2c55be336d88c19
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-08-09 02:30:39 -0600

    [add] Local bootstrap libraries

--- api-payroll/public/html/login.php
+++ api-payroll/public/html/login.php
@@ -1,11 +1,11 @@
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
 
 <!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../js/jquery.min.js"></script>
 
 <!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
 
 <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <div class="container">
